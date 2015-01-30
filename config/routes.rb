ff9d6453de7ff46207c1d53e40956a758eade6ce
dodi <dodisabaruddin@gmail.com>
2015-01-31 01:13:49 +0700

    Contest Functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  
 
   resources :orders do 
     collection do 
@@ -11,8 +10,9 @@ Rails.application.routes.draw do
     collection do 
       get 'import'
     end
+    resources :variants
   end
-  resources :variants
+  
 
   resources :accounts do 
     member do 
---
+++ spec/factories/contests.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :contest do
+    name "MyString"
+product_id 1
+start_date "2015-01-30 20:34:16"
+end_date "2015-01-30 20:34:16"
+max_result 1
+order_id 1
+  end
+
+end
